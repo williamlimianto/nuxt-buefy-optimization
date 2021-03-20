a398d999ebef3aaec24e4676632ae90aa6dd2969
William Limianto <william.limianto@gmail.com>
2021-03-20 11:00:16 +0700

    Initial sele-sele commit.

---
+++ plugins/buefy.ts
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import { Buefy } from 'buefy'
+
+Vue.use(Buefy)
+
+// import { Button, Icon, Toast, Dialog } from 'buefy'
+
+// Vue.use(Button)
+// Vue.use(Icon)
+// Vue.use(Toast)
+// Vue.use(Dialog)
